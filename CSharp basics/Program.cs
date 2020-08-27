4b53cc586e04f00810febd433bea322c3d221ecb
Lockner_Hughston <Lockner_Hughston@edu.mcctc.net>
2020-08-27 10:20:20 -0400

    Add project files.

---
+++ CSharp basics/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.ComponentModel.DataAnnotations;
+
+namespace CSharp_basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MyString();
+
+            Console.ReadLine();
+        }
+
+        static void MyString()
+        {
+            Console.Write("What year were you born?");
+            int yearBorn = Convert.ToInt32(Console.ReadLine());
+            Console.Write("What is your age?");
+            string myName = Console.ReadLine();
+
+            Console.WriteLine($"Your age is: {myName}, and you were born in {yearBorn}");
+
+            string myString1 = "This is my first String";
+            string theTruth = "The last thing a ninja would do is admit to being a ninja.";
+            string theLie = "I am not a ninja.";
+            Console.WriteLine("This is my string. " + myString1);
+            Console.WriteLine($"Here are two truths and a lie. {theTruth}, {theLie}");
+        }
+    }
+}
